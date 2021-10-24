719b27422210871cfecd6ca8c4748fcf774d1ae7
Pete Kneller <pommypete@gmail.com>
2021-10-24 12:31:28 +0100

    Adds inheritance between loadouts

--- generate_loadout.py
+++ generate_loadout.py
@@ -13,16 +13,17 @@ config_dir_name = sys.argv[1]
 output_dir_name = sys.argv[2]
 
 def read_config(dir_entry):
-    print(f"Reading: {dir_entry}")
+    print(f"Reading [{dir_entry}]")
 
     try:
         with open(dir_entry, "r") as config_file:
-            return yaml.safe_load(config_file)
+            config = yaml.safe_load(config_file)
+            return (config['name'], config)
 
     except Exception as exc:
-        print(f"Exception raised in {dir_entry}: {exc}")
+        print(f"Exception raised in [{dir_entry}]: {exc}")
 
-configs = [read_config(entry) for entry in Path(config_dir_name).iterdir()]
+configs = dict([read_config(entry) for entry in Path(config_dir_name).iterdir()])
 env = Environment(
     loader=FileSystemLoader("."),
     keep_trailing_newline=True,
@@ -31,8 +32,32 @@ env = Environment(
 )
 template = env.get_template("arsenal_import.sqf.tmpl")
 
-for config in configs:
-    file_name = Path(output_dir_name) / config['name']
+def merge_configs(parent, child):
+    out = parent.copy()
+    for key in iter(child):
+        if key not in out:
+            out[key] = child[key]
+        elif isinstance(out[key], dict):
+            out[key] = merge_configs(out[key], child[key])
+        else:
+            out[key] = child[key]
+
+    return out
+
+def fetch_config(config_name):
+    config = configs[config_name]
+    if "extends" in config:
+        print(f"Config {config_name} extends [{config['extends']}]")
+        base_config = fetch_config(config['extends'])
+        print(f"Merging base config into [{config_name}]")
+        return merge_configs(base_config, config)
+    else:
+        return config
+
+for config_name in iter(configs):
+    file_name = Path(output_dir_name) / f"{config_name}.sqf"
     with open(file_name, "w") as output_file:
-        print(f"Writing loadout: {file_name}")
+        print(f"Processing config [{config_name}]")
+        config = fetch_config(config_name)
+        print(f"Writing loadout [{file_name}]")
         output_file.write(template.render(config=config))
