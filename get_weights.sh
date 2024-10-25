6bc883cd11904fdee1c6d057ab94032513bae0cd
Pete Kneller <pommypete@gmail.com>
2024-10-25 17:42:31 +0100

    Corrects the weight merging script
    It was using `add` to reduce the array of duplicate objects. `add` uses
    `+` under the hood as the reducer and `+` doesn't deep merge objects -
    right-most wins. I instead need to reduce explicitly using the `*`
    (merge) operator.

--- get_weights.sh
+++ get_weights.sh
@@ -1,4 +1,4 @@
 yq -y 'to_entries | sort_by(.key) | from_entries' <from_get_weights.sqf>.yaml 
 
 # or to merge
-yq -sy 'map(to_entries) | flatten | group_by(.key) | map(add) | from_entries' weights.yaml <from_get_weights.sqf>.yaml > <output_file>.yaml
+yq -sy 'map(to_entries) | flatten | group_by(.key) | map(reduce .[] as $i ({}; $i * .)) | from_entries' weights.yaml <from_get_weights.sqf>.yaml > <output_file>.yaml
